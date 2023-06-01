abc9fe4d4345e634148d88b4e478e06fc0336093
MrGameandCode <111073646+MrGameandCode@users.noreply.github.com>
2023-06-01 20:29:51 +0200

    cypress eslint for 11.9

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({// eslint-disable-line no-unused-vars
+module.exports = defineConfig({ // eslint-disable-line no-unused-vars
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) { // eslint-disable-line no-unused-vars
       // implement node event listeners here
     },
   },
